0e067c9c95bc9e790b067931d587578bcec03828
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-05-27 14:30:46 +0200

    adding js example

---
+++ www/script.js
@@ -0,0 +1,53 @@
+alert('Hello World !');
+
+console.log('Hello World !');
+
+var helloWorld = 'Hello World !';
+
+console.log(helloWorld);
+
+var hello = 'Hello ';
+
+console.log(hello + 'World !');
+
+var text = 'text';
+var number = 1;
+var boolean = true;
+
+console.log(1 + 1);
+var result = 1 + 1;
+console.log(result);
+result = result * 2;
+console.log(result);
+
+if (true) {
+  console.log('this is true');
+}
+
+if (false) {
+  console.log('this is true');
+}
+else {
+  console.log('this is false');
+}
+
+if (1 + 1 == 2) {
+  console.log('one and one is two');
+}
+
+if (1 - 1 == 2) {
+  console.log('one minus one is two');
+}
+else {
+  console.log('one minus one is not two');
+}
+
+var counter = 0;
+while (counter < 10) {
+  console.log(counter);
+  counter++;
+}
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
