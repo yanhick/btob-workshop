05828738ebd11cf1bb9b5d8b0b4ca52e51ba2b69
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-05-27 14:58:53 +0200

    update js

--- www/script.js
+++ www/script.js
@@ -31,11 +31,11 @@ else {
   console.log('this is false');
 }
 
-if (1 + 1 == 2) {
+if (1 + 1 === 2) {
   console.log('one and one is two');
 }
 
-if (1 - 1 == 2) {
+if (1 - 1 === 2) {
   console.log('one minus one is two');
 }
 else {
@@ -51,3 +51,49 @@ while (counter < 10) {
 for (var i = 0; i < 10; i++) {
   console.log(i);
 }
+
+
+function log(message) {
+  console.log(message);
+}
+
+
+var array = [];
+array[0] = 'my first element';
+array[1] = 'my second element';
+
+log(array[0]);
+log(array[1]);
+log(array);
+
+for (var index = 0; index < array.length; index++) {
+  log(array[index]);
+}
+
+var me = {
+  name: 'Yannick',
+  job: 'developement'
+};
+
+log(me.name);
+log(me.job);
+log(me);
+
+function whoAmI (person) {
+  return 'I am ' + person.name + ', I do ' + person.job;
+}
+
+log(whoAmI(me));
+
+
+var contactForm = document.getElementById('contact');
+var textField = document.getElementById('message');
+
+contactForm.onsubmit = function() {
+  if (textField.value === '') {
+    alert('Vous devez mettre un message !');
+  }
+}
+
+
+
